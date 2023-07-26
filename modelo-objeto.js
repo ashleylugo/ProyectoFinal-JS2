2460ab527cbadb2f95bc98736ab9f18ebce0b592
Ashley Lugo <acmlugoramirez@gmail.com>
2023-07-26 20:40:27 -0300

    Proyecto

---
+++ modelo-objeto.js
@@ -0,0 +1,17 @@
+class Clase {
+    nombre;
+    profesor;
+    dificultad;
+    barrio;
+    horario;
+
+
+constructor(nombre, profesor, dificultad, barrio, horario){
+    this.nombre = nombre;
+    this.profesor = profesor;
+    this.dificultad = dificultad;
+    this.barrio = barrio;
+    this.horario = horario;
+
+    }
+}
